e762bec93770286addf59c3f3c1470d25b1542e3
Arsan <flikie3@gmail.com>
2020-11-28 19:53:32 +0900

    добавил логику сплеша

--- app/src/main/java/com/example/rosseti/SplashScreen.kt
+++ app/src/main/java/com/example/rosseti/SplashScreen.kt
@@ -1,15 +1,24 @@
 package com.example.rosseti
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rosseti.presentation.SplashPresenter
 import com.example.rosseti.presentation.SplashView
 import dagger.hilt.android.AndroidEntryPoint
+import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
+import javax.inject.Inject
+import javax.inject.Provider
 
 @AndroidEntryPoint
-class SplashScreen : AppCompatActivity(), SplashView {
+class SplashScreen : MvpAppCompatActivity(), SplashView {
+
+    @Inject
+    lateinit var presenterProvider: Provider<SplashPresenter>
+
+    @ProvidePresenter
+    fun providePresenter(): SplashPresenter = presenterProvider.get()
 
     @InjectPresenter
     lateinit var presenter: SplashPresenter
@@ -17,18 +26,13 @@ class SplashScreen : AppCompatActivity(), SplashView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        // проверка токена авторизации
-        val intent = if (checkToken()) {
-            Intent(this, MainActivity::class.java)
-        } else {
-            Intent(this, AuthorizationActivity::class.java)
-        }
-
-        startActivity(intent)
     }
 
-    private fun checkToken(): Boolean {
-        return false
+    override fun goToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun goToLogin() {
+        startActivity(Intent(this, AuthorizationActivity::class.java))
     }
 }
--- app/src/main/java/com/example/rosseti/presentation/SplashView.kt
+++ app/src/main/java/com/example/rosseti/presentation/SplashView.kt
@@ -6,4 +6,6 @@ import moxy.viewstate.strategy.StateStrategyType
 
 @StateStrategyType(AddToEndSingleStrategy::class)
 interface SplashView : MvpView {
+    fun goToMain()
+    fun goToLogin()
 }
