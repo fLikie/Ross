bf9479e3693a2b7266233414158896b63f97b773
Maksim Korovin <xlit97@yandex.ru>
2020-11-29 12:45:34 +0900

    Добавил генератор ранд для карточек

--- app/src/main/java/com/example/rosseti/ui/ProblemsAdapter.kt
+++ app/src/main/java/com/example/rosseti/ui/ProblemsAdapter.kt
@@ -3,11 +3,16 @@ package com.example.rosseti.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rosseti.R
+import kotlin.random.Random
 
 class ProblemsAdapter : RecyclerView.Adapter<ProblemsAdapter.ProblemsVH>() {
     val data = listOf(1,2,3,4,5)
+
+    val nameList = listOf("Николай", "Сергей", "Владимир", "Иван", "Петр")
+    val lastNameList = listOf("Петров", "Иванов", "Кузнецов", "Егоров", "Максимов")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProblemsVH {
         return ProblemsVH(LayoutInflater.from(parent.context).inflate(R.layout.item_card_idea, parent, false))
@@ -20,10 +25,19 @@ class ProblemsAdapter : RecyclerView.Adapter<ProblemsAdapter.ProblemsVH>() {
     override fun getItemCount(): Int = data.size
 
     inner class ProblemsVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ratingTextView: TextView = itemView.findViewById(R.id.rating_score)
+        val nameTextView: TextView = itemView.findViewById(R.id.name)
+        val dateTextView: TextView = itemView.findViewById(R.id.publication_date)
+        val viewsCountTextView: TextView = itemView.findViewById(R.id.views_counter)
 
 
         fun bind(position: Int) {
-
+            val randomRating = Random.nextInt(0,99)
+            ratingTextView.text = randomRating.toString()
+            nameTextView.text = "${lastNameList.random()} ${nameList.random()}"
+            val daysText = if (position == 0) "день" else if (position in 1..4) "дня" else "дней"
+            dateTextView.text = "${(position + 1)} $daysText назад"
+            viewsCountTextView.text = (randomRating + 13).toString()
         }
     }
 }
--- app/src/main/java/com/example/rosseti/ui/ReestrFragment.kt
+++ app/src/main/java/com/example/rosseti/ui/ReestrFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rosseti.R
+import com.example.rosseti.databinding.FragmentProblemsBinding
 
 class ReestrFragment : Fragment() {
+    private var _binding: FragmentProblemsBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reestr, container, false)
+    ): View {
+        _binding = FragmentProblemsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ideasRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.ideasRecyclerView.adapter = ProblemsAdapter()
     }
 }
